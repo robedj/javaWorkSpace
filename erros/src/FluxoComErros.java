ffbcd1c91f556f070b0a9e6cb54095a250f7ba07
robedj <ed.wassouf@gmail.com>
2021-11-13 16:54:49 -0400

    utilizando throws

--- erros/src/FluxoComErros.java
+++ erros/src/FluxoComErros.java
@@ -5,7 +5,7 @@ public class FluxoComErros {
         System.out.println("Inicio da main");
         try {
             metodo1();
-        } catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
+        } catch(ArithmeticException | NullPointerException | MinhaExcecao ex) { //Tratamento nao necessita throws
             String msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -13,15 +13,15 @@ public class FluxoComErros {
         System.out.println("Fim da main");
     }
 
-    public static void metodo1() {
+    public static void metodo1() throws MinhaExcecao {
         System.out.println("inicio do metodo 1");
         metodo2();
         System.out.println("Fim do metodo 1");
     }
-
-    public static void metodo2() {
+//exige que coloca na assinatura do metodo
+    public static void metodo2() throws MinhaExcecao {
         System.out.println("chamando metodo 2");
-        metodo2();
-        System.out.println("fim do metodo 2");
+        throw new MinhaExcecao("deu muito errado");
+        //System.out.println("fim do metodo 2");
     }
 }
