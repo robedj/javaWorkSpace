431a5fd1e377c03d5bfaedfd6333eade2a8b0bd2
robedj <ed.wassouf@gmail.com>
2021-11-17 17:00:54 -0400

    Excecao Unchecked Revisada

--- bankHerdadoConta/src/TestaSaca.java
+++ bankHerdadoConta/src/TestaSaca.java
@@ -1,6 +1,7 @@
 public class TestaSaca {
     public static void main(String[] args) {
         Conta conta = new ContaCorrente(123, 321);
+
         conta.deposita(200);
         conta.saca(210);
         //Sai abruptamente sem chegar no final
