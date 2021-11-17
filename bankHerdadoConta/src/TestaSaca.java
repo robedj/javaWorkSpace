d31a410f87fd2d75133e325e11b97eb2ec539dc8
robedj <ed.wassouf@gmail.com>
2021-11-17 16:56:48 -0400

    Excecao para valores maiores que saldo

--- bankHerdadoConta/src/TestaSaca.java
+++ bankHerdadoConta/src/TestaSaca.java
@@ -2,7 +2,8 @@ public class TestaSaca {
     public static void main(String[] args) {
         Conta conta = new ContaCorrente(123, 321);
         conta.deposita(200);
-        conta.saca(190);
+        conta.saca(210);
+        //Sai abruptamente sem chegar no final
 
         System.out.println(conta.getSaldo());
     }
