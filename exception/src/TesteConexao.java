d17ffabaee9a35c341cd6a8a3545f7418a377c66
robedj <ed.wassouf@gmail.com>
2021-11-17 18:16:15 -0400

    try-catch-finally

--- exception/src/TesteConexao.java
+++ exception/src/TesteConexao.java
@@ -1,12 +1,15 @@
 public class TesteConexao {
     public static void main(String[] args) {
-
+        Conexao con = null;
         try{
-            Conexao con = new Conexao();
+            con = new Conexao();
             con.leDados();
             con.fecha();
         }catch(IllegalStateException ex){
             System.out.println("Deu erro na conexao");
+
+        }finally {
+            con.fecha();
         }
 
     }
